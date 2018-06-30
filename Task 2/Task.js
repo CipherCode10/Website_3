9fdf7dc0185675c5ed1527d2e36fbb10c7a756b8
Cipher_Code <Cipher_Code91@protonmail.com>
2018-06-30 14:38:46 -0700

    This is Modified Website

---
+++ Task 2/Task.js
@@ -0,0 +1,31 @@
+/*  Created by : Ahmed Yassen  */
+
+function Sinlog()
+{
+  window.open("SL.HTML");
+}
+
+function EXC()
+{
+  document.getElementById("p1").innerHTML="Excellent";
+}
+
+function VG()
+{
+  document.getElementById("p2").innerHTML="Above Average";
+}
+
+function VV()
+{
+  document.getElementById("p3").innerHTML="Average";
+}
+
+function GG()
+{
+  document.getElementById("p4").innerHTML="Below Average";
+}
+
+function PP()
+{
+  document.getElementById("p5").innerHTML="Poor";
+}
